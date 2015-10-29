fe70e3f61f02b8bbd1a3fe2a134a9768634498c0
jlgarrido <jlgarrido932@gmail.com>
2015-10-29 09:57:40 +0100

    commit 2

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -6,7 +6,8 @@ namespace HolaMundo
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Hola Mundo");
+			Console.WriteLine ("Que tal ");
 		}
 	}
 }
